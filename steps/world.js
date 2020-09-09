595254db50ccc018847b084ca261e1fffe2332a9
Philipp Mudra <PMudra@users.noreply.github.com>
2020-09-09 12:56:52 +0200

    Adding edge to build matrix (#1)

--- steps/world.js
+++ steps/world.js
@@ -2,6 +2,7 @@ const {setWorldConstructor} = require("cucumber");
 const {Builder} = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const firefox = require('selenium-webdriver/firefox');
+const edge = require('selenium-webdriver/edge');
 
 // variables & methods of this class can be access by any step definition
 // see https://github.com/cucumber/cucumber-js/blob/master/docs/support_files/world.md
@@ -19,10 +20,13 @@ class BrowserWorld {
         chromeOptions.addArguments("--no-sandbox", "--disable-dev-shm-usage");
         const firefoxOptions = new firefox.Options();
         firefoxOptions.headless();
+        const edgeOptions = new edge.Options();
+        edgeOptions.setEdgeChromium(true);
         this.driver = new Builder()
             .forBrowser('chrome')
             .setChromeOptions(chromeOptions)
             .setFirefoxOptions(firefoxOptions)
+            .setEdgeOptions(edgeOptions)
             .build();
     }
 
