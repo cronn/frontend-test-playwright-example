0b16fdc07096ba6563a9cea936826a3c2e54eaff
Sebastian Hoß <sebastian.hoss@cronn.de>
2020-09-09 14:12:56 +0200

    increase timeout for slow test runners

--- steps/browser_steps.js
+++ steps/browser_steps.js
@@ -4,6 +4,7 @@ Given('a browser is open', async function () {
     await this.initBrowser();
 });
 
-After(async function () {
+// increase timeout for slow test runners
+After({timeout: 10 * 5000}, async function () {
     await this.driver.quit();
 });
