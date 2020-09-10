14524a463186cffdc8498fdd4c423e4d4d48ff67
Philipp Mudra <philipp.mudra@web.de>
2020-09-10 22:11:54 +0200

    Set cucumber default timeout to 60 seconds

--- steps/world.js
+++ steps/world.js
@@ -1,5 +1,9 @@
-const { setWorldConstructor } = require("cucumber");
+const { setWorldConstructor, setDefaultTimeout } = require("cucumber");
 const { chromium, webkit, firefox } = require("playwright");
+
+// By default, cucumbers asynchronous hooks and steps timeout after 5000 milliseconds.
+// Playwrights timeout defaults to 30 seconds, so we are setting cucumbers timeout twice as high.
+setDefaultTimeout(60 * 1000);
 
 // variables & methods of this class can be access by any step definition
 // see https://github.com/cucumber/cucumber-js/blob/master/docs/support_files/world.md
